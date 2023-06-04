242146419d9461c0b19cf166eed20e14e2b5f2b8
alif5218 <128446391+alif5218@users.noreply.github.com>
2023-06-04 20:15:33 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// function on(){
+//     document.getElementById('image').src='80bdb25f4cd1ac2d9666428f37553932.gif';
+// }
+// function off(){
+//     document.getElementById('image').src='80bdb25f4cd1ac2d9666428f37553932ff.gif';
+// }
+
+
+
+// function on(){
+//     document.getElementById('image').src='lampard.gif';
+// }
+// function off(){
+//     document.getElementById('image').src='lampardff.gif';
+// }
+
+function convert(){
+    var p = document.getElementById('gt');
+
+    p.classList.toggle('active');
+}
+
